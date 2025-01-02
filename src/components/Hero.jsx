a35a480d6eb4689ea0a2bc43eaea38eea03d32fa
Sariah Jackson <sarijah13@gmail.com>
2025-01-02 15:59:34 -0400

    update hero animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,18 +4,27 @@ import { useGSAP } from "@gsap/react";
 
 export default function Hero() {
   useGSAP(()=>{
+
+    gsap.fromTo(".cta-1", {x: -20, opacity: 0}, {x: 0, opacity: 1, duration: 0.75 })
+    gsap.fromTo(".cta-2", {y: 20, opacity: 0}, {y: 0, opacity: 1, duration: 0.75}, "<50%")
+    gsap.fromTo(".cta-3", {x: 20, opacity: 0}, {x: 0, opacity: 1, duration: 0.75}, "<50%")
+    gsap.fromTo(".cta-4", {y: 20, opacity: 0}, {y: 0, opacity: 1, duration: 0.75}, "<50%")
+    gsap.fromTo(".cta-5", {x: 20, opacity: 0}, {x: 0, opacity: 1, duration: 0.75}, "<50%")
+
     gsap.fromTo(".hero-text",
       {
         scale: 0.5,
+        y: 20,
         opacity: 0
       },
       {
         scale: 1,
+        y: 0,
         opacity: 1,
-        duration: 1,
+        duration: 1.2,
         stagger: 0.7,
         ease: "power3.inOut"
-      }
+      }, "<95%"
     )
 
     gsap.fromTo(".hero-button",
@@ -26,10 +35,10 @@ export default function Hero() {
       {
         y: 0,
         opacity: 1,
-        duration: 1,
+        duration: 1.5,
         stagger: 0.3,
         ease: "back.inOut"
-      }, "<99%"
+      }, "<90%"
     )
 
   })
@@ -37,7 +46,19 @@ export default function Hero() {
     <section className='h-[100vh] flex items-center justify-center'>
         <div className='text-center flex flex-col gap-10 items-center'>
            <div className='relative z-10 flex flex-col gap-2'>
-            <h1 className='hero-text leading-tight text-[2.7rem] lg:text-6xl font-bold'>Big Results for <br />Small Businesses</h1>
+            
+            <div className='text-[2.7rem] lg:text-6xl font-bold flex flex-col items-center'>
+              <div className='cta-text flex gap-3'>
+                <h1><span className='cta-1'>Big </span></h1>
+                <h1><span className='cta-2'>Results </span></h1>
+                <h1><span className='cta-3'>For</span></h1>
+              </div>
+              <div className='cta-text flex gap-3 -mt-3 lg:-mt-1'>
+                <h1><span className='cta-4'>Small </span></h1>
+                <h1><span className='cta-5'>Businesses </span></h1>
+              </div>
+            </div>
+            
             <p className='hero-text text-sm px-10'>Affordable, impactful marketing tailored to help your business grow.</p>
            </div>
 
